4cd41f6fb29e4f782357e185a8b375b5c2993c59
Dev Patel <devpatel1828king@gmail.com>
2025-02-18 18:53:54 +0530

    move chat_screen to test folder

--- test/users_list_screen.dart
+++ test/users_list_screen.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../lib/chat_screen.dart';
+import 'chat_screen.dart';
 
 class UsersListScreen extends StatefulWidget {
   const UsersListScreen({Key? key}) : super(key: key);
